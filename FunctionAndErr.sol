a1f57d7ebc72a065c09e26b85a26fd95d6dc8ed8
Vaishnav Arora <vaishgabha@gmail.com>
2023-08-05 01:29:00 +0530

    Update FunctionAndErr.sol

--- FunctionAndErr.sol
+++ FunctionAndErr.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+contract ExceptionTrial {
+    uint256 public value;
+
+    function setValue(uint256 _newValue) external {
+        require(_newValue > 0, "Value must be greater than 0");
+        
+        value = _newValue;
+    }
+
+    function assertExample(uint256 _input) external pure returns (uint256) {
+        assert(_input > 10);
+        return _input;
+    }
+
+    function revertExample(uint256 _input) external pure returns (uint256) {
+        if (_input == 0) {
+            revert("Input cannot be zero");
+        }
+        
+        return _input;
+    }
+}
